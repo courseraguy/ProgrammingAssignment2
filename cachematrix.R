3b2f72bcf439da56ac69a7a0971b1cab26983f28
courseraguy <chrispcoffin+edu@gmail.com>
2014-04-27 14:28:50 -0400

    Implemented clever performance optimization

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+## Matrix that can cache its inverse
 
-## Write a short comment describing this function
-
+## Creates a "matrix" that can cache its inverse ("solve")
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## initialize cache
+    s <- NULL
+    
+    ## create get/set functions
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setSolve <- function(inverse) {
+        s <<- inverse
+    }
+    getSolve <- function() {
+        s
+    }
+    
+    ## return the functions
+    list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
+## Calculates and caches inverse, or returns the cached inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check for cached inverse
+    s <- x$getSolve()
+    if (is.null(s)) {
+        ## No cached inverse; calculate and cache it
+        s <- solve(x$get())
+        x$setSolve(s)
+    } else {
+        ## Cached inverse found
+        message("returning cached data")
+    }
+    ## Return the inverse
+    s
 }
